279f5e2c74393b2fd8ef7444e4d3f30f03b838c6
tom <tom@163.com>
2020-07-31 11:09:53 +0800

    111111

---
+++ src/main/java/com/hr/bean/stuctureTest.scala
@@ -0,0 +1,13 @@
+package com.hr.bean
+
+/**
+  * HF
+  * 2020-06-20 22:13
+  */
+case class stuctureTest(
+                         auth :String,
+                         id :String,
+                         var modifyFlag :String
+                       ) {
+
+}
